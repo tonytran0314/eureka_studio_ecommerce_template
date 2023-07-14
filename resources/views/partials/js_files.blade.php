08c2c01b5d6a6f35a837c15512f6ba2bba301d25
Gia Huy Tran <tonytran0314@gmail.com>
2023-07-14 15:53:02 +0700

    fix js not loading

--- resources/views/partials/js_files.blade.php
+++ resources/views/partials/js_files.blade.php
@@ -3,9 +3,9 @@
 
 
 
-<script src="{{ asset('js/login.js') }}"></script>
-<script src="{{ asset('js/add_to_cart.js') }}"></script>
-<script src="{{ asset('js/show_item_in_cart.js') }}"></script>
-<script src="{{ asset('js/remove_item_from_cart.js') }}"></script>
-<script src="{{ asset('js/item_quantity_adjustment.js') }}"></script>
-<script src="{{ asset('js/dropdown_toggle.js') }}"></script>
+<script src="{{ secure_asset('js/login.js') }}"></script>
+<script src="{{ secure_asset('js/add_to_cart.js') }}"></script>
+<script src="{{ secure_asset('js/show_item_in_cart.js') }}"></script>
+<script src="{{ secure_asset('js/remove_item_from_cart.js') }}"></script>
+<script src="{{ secure_asset('js/item_quantity_adjustment.js') }}"></script>
+<script src="{{ secure_asset('js/dropdown_toggle.js') }}"></script>
